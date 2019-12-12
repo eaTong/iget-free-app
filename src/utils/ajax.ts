8d3ffad564861125eec2e8f4028f7c3e9161a1e8
eaTong <eatongchou@gmail.com>
2019-12-12 17:42:32 +0800

    add plugin : http for request in ios

--- src/utils/ajax.ts
+++ src/utils/ajax.ts
@@ -1,5 +1,7 @@
-import axios, {Method} from 'axios';
+import axios, { Method } from 'axios';
 import showToast from './toastUtil';
+import { isPlatform } from '@ionic/react';
+import { HTTP } from '@ionic-native/http';
 
 interface AjaxConfig {
   url: string,
@@ -8,32 +10,58 @@ interface AjaxConfig {
 }
 
 export default function ajax(config: AjaxConfig): Promise<any> {
-  const {url, data, method} = config;
+  const { url, data, method } = config;
+  const urlPrefix = 'https://iget.eatong.cn';
   return new Promise(((resolve, reject) => {
-    axios(
-      {
-        withCredentials:true,
-        url: 'https://iget.eatong.cn' + url,
-        data,
-        method: method || 'POST'
-      }
-    ).then((result: any) => {
-      if (result.data.success) {
-        resolve(result.data.data);
-      } else {
-        showToast(`请求错误:${result.data.message}`);
-        reject();
-      }
-    }).catch((reason: any) => {
-      console.log(reason);
-      try{
-        const error =  reason.response.data.message;
-        showToast(`${error}`);
-        reject();
-      }catch (e) {
-        reject();
-      }
+    if (isPlatform('ios')) {
+      HTTP.sendRequest(urlPrefix + url, { method: 'post', data }).then((result: any) => {
+        if (result.status === 200) {
+          const resultData = JSON.parse(result.data);
+          console.log(resultData);
+          if (resultData.success) {
+            resolve(resultData.data);
+          } else {
+            showToast(`${resultData.message}`);
+            reject();
+          }
+        } else {
+          reject();
+        }
+      }).catch((reason: any) => {
+        try {
+          const error = JSON.parse(reason.error).message;
+          showToast(`${error}`);
+          reject();
+        } catch (e) {
+          reject();
+        }
+      })
+    } else {
+      axios(
+        {
+          withCredentials: true,
+          url: urlPrefix + url,
+          data,
+          method: method || 'POST'
+        }
+      ).then((result: any) => {
+        console.log(result);
+        if (result.data.success) {
+          resolve(result.data.data);
+        } else {
+          showToast(`${result.data.message}`);
+          reject();
+        }
+      }).catch((reason: any) => {
+        try {
+          const error = reason.response.data.message;
+          showToast(`${error}`);
+          reject();
+        } catch (e) {
+          reject();
+        }
 
-    })
+      })
+    }
   }))
 }
